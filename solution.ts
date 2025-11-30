4a5b194e4f7d86ff8e35f5526ac39ee5c52d702e
Muhammad Sanim <sanim@Sanims-MacBook-Air.local>
2025-11-30 12:44:59 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,13 @@
+type FormatValueType = string | number | boolean;
+
+const formatValue = (value: FormatValueType): FormatValueType => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
+
+
